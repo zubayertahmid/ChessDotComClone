455e5542328cca2822b82d4ffd8c680e99fab4f9
zubayertahmid <zubayertahmid2218@gmail.com>
2024-07-08 12:13:12 +0600

    Chessboard setup completed

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,5 +1,3 @@
-const { render } = require("ejs");
-
 const socket = io();
 const chess = new Chess();
 const boardElement = document.querySelector(".chessboard");
@@ -13,15 +11,28 @@ const renderBoard = () =>{
     boardElement.innerHTML = "";
     board.forEach((row, rowindex) => {
         row.forEach((square, squareindex) => {
-            console.log(square);
+           const squareElement = document.createElement("div");
+           squareElement.classList.add("square", 
+            (rowindex + squareindex)%2 === 0 ? "light" : "dark"
+           );
+
+           squareElement.dataset.row = rowindex;
+           squareElement.dataset.col = squareindex;
+        
+           if(square){
+            const pieceElement = document.createElement("div");
+            pieceElement.classList.add("piece", 
+            square.color === 'w' ? "white" : "black");
+
+            pieceElement.innerText = "";
+            pieceElement.draggable = playerRole === square.color;
+           }
         })
     });
 };
 const handleMove = () =>{};
 const getPieceUnicode = () => {};
  
-
-
 renderBoard();
 
 
